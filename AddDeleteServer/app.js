290250b4e8455aa8ece9fb25586c359d1b0a2ac7
Vishal Kohli <61668514+vishal-kohli@users.noreply.github.com>
2020-08-22 16:26:53 -0400

    AddDelete Server & /delete /add API

--- AddDeleteServer/app.js
+++ AddDeleteServer/app.js
@@ -1,11 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3001;
+const bodyParser = require('body-parser');
+const redis = require('redis')
+const redis_client = redis.createClient()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(bodyParser.json());
+
+
+// Method will delete list of incoming product names;
+app.post('/delete', (req, res) => {
+    const toBeDeleted = req.body.productNames;
+
+    for (i = 0; i < toBeDeleted.length; i++) {
+        redis_client.DEL(toBeDeleted[i]);
+    }
+    res.send("SUCCESS");
+});
+
+//Method will add list of incoming products;
+app.post('/add', (req, res) => {
+
+    const productName = req.body.productName;
+    const productQuantity = req.body.productQuantity;
+    redis_client.set(productName, productQuantity, function (err, reply) {
+        if (reply === "OK") {
+            res.send("SUCCESS");
+        }
+        else {
+            throw new Error("Something went wrong. Try again.");
+        }
+    })
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
