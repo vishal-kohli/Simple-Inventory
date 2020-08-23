4d4ebb2d152849be5ca9563dc3bb97cb14a291fd
Vishal Kohli <61668514+vishal-kohli@users.noreply.github.com>
2020-08-23 15:23:26 -0400

    integrated mysql db

--- AddDeleteServer/app.js
+++ AddDeleteServer/app.js
@@ -4,35 +4,79 @@ const port = 3001;
 const bodyParser = require('body-parser');
 const redis = require('redis')
 const redis_client = redis.createClient()
+const mysql = require('mysql');
 
 app.use(bodyParser.json());
 
-
-// Method will delete list of incoming product names;
-app.post('/delete', (req, res) => {
-    const toBeDeleted = req.body.productNames;
-
-    for (i = 0; i < toBeDeleted.length; i++) {
-        redis_client.DEL(toBeDeleted[i]);
-    }
-    res.send("SUCCESS");
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    database: 'mysql_db'
 });
 
-//Method will add list of incoming products;
-app.post('/add', (req, res) => {
-
-    const productName = req.body.productName;
-    const productQuantity = req.body.productQuantity;
-    redis_client.set(productName, productQuantity, function (err, reply) {
-        if (reply === "OK") {
-            res.send("SUCCESS");
-        }
-        else {
-            throw new Error("Something went wrong. Try again.");
+pool.on('connection', function (connection) {
+    connection.query('CREATE TABLE IF NOT EXISTS products (name TEXT, quantity SMALLINT)', function (err, res) {
+        if (err) {
+            console.log(err);
         }
     })
 });
 
+// Method will delete list of incoming product names;-----------------
+app.post('/delete', (req, res) => {
+    const toBeDeleted = req.body.productNames;
+
+    for (i = 0; i < toBeDeleted.length; i++) {
+        redis_client.EXISTS(toBeDeleted[i], function (err, res) {
+            console.log(res);
+
+            // if exists - delete from redis and mysql
+            if (res > 0) {
+                redis_client.DEL(toBeDeleted[i], function (err, res) {
+                    pool.query(`DELETE FROM products WHERE name ="${toBeDeleted[i]}"`, function (err, res) {
+                        if (err) {
+                            console.log(err);
+                            res.send({ "operation_successful": false });
+                        }
+                        else {
+                            res.send({ "operation_successful": true });
+                        }
+                    });
+                });
+            }
+        })
+    }
+
+});
+
+//Method will add list of incoming products;---------------------
+app.post('/add', (req, res) => {
+    const productName = req.body.productName;
+    const productQuantity = req.body.productQuantity;
+
+    //insert into redis
+    redis_client.set(productName, productQuantity, function (err, reply) {
+        if (err) {
+            console.log(err);
+            res.send({ "operation_successful": false });
+        }
+
+        // insert into mysql
+        else {
+            pool.query('INSERT INTO products SET ?', data, function (err, result) {
+                if (err) {
+                    console.log(err);
+                    res.send({ "operation_successful": false });
+                }
+                else {
+                    console.log(result);
+                    res.send({ "operation_successful": true });
+                }
+            })
+        }
+    });
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Inventory is not listening at http://localhost:${port}`);
 });
