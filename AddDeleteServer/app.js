92960746542642c1aef09cae55649faec36a9822
Vishal Kohli <61668514+vishal-kohli@users.noreply.github.com>
2020-08-23 23:18:51 -0400

    Delete and Add APIs complete

--- AddDeleteServer/app.js
+++ AddDeleteServer/app.js
@@ -10,12 +10,15 @@ app.use(bodyParser.json());
 
 const pool = mysql.createPool({
     connectionLimit: 10,
+    user: 'root',
+    password: 'password',
     host: 'localhost',
-    database: 'mysql_db'
+    port: '3306',
+    database: 'inventory_db'
 });
 
 pool.on('connection', function (connection) {
-    connection.query('CREATE TABLE IF NOT EXISTS products (name TEXT, quantity SMALLINT)', function (err, res) {
+    connection.query('CREATE TABLE IF NOT EXISTS products (name varchar(30) PRIMARY KEY, quantity SMALLINT)', function (err, res) {
         if (err) {
             console.log(err);
         }
@@ -26,26 +29,41 @@ pool.on('connection', function (connection) {
 app.post('/delete', (req, res) => {
     const toBeDeleted = req.body.productNames;
 
-    for (i = 0; i < toBeDeleted.length; i++) {
-        redis_client.EXISTS(toBeDeleted[i], function (err, res) {
-            console.log(res);
+    let response = {
+        "operation_successful": false,
+        "error": null
+    };
+
+    for (let i = 0; i < toBeDeleted.length; i++) {
+
+        redis_client.EXISTS(toBeDeleted[i], function (err, result) {
 
             // if exists - delete from redis and mysql
-            if (res > 0) {
-                redis_client.DEL(toBeDeleted[i], function (err, res) {
-                    pool.query(`DELETE FROM products WHERE name ="${toBeDeleted[i]}"`, function (err, res) {
-                        if (err) {
-                            console.log(err);
-                            res.send({ "operation_successful": false });
-                        }
-                        else {
-                            res.send({ "operation_successful": true });
-                        }
-                    });
+            if (result > 0) {
+                redis_client.del(toBeDeleted[i], function (err, result) {
+                    if (err) {
+                        console.log(err)
+                        response.error = err;
+                        res.send(response);
+                    }
+                    else {
+                        pool.query(`DELETE FROM products WHERE name ="${toBeDeleted[i]}"`, function (err, result) {
+                            if (err) {
+                                console.log(err)
+                                response.error = err;
+                                res.send(response);
+                            }
+                            else {
+                                response.operation_successful = true;
+                                res.send(response);
+                            }
+                        });
+                    }
                 });
             }
         })
     }
+    // res.send(null);
 
 });
 
@@ -54,29 +72,57 @@ app.post('/add', (req, res) => {
     const productName = req.body.productName;
     const productQuantity = req.body.productQuantity;
 
+    let response = {
+        "operation_successful": false,
+        "error": null
+    };
+
     //insert into redis
     redis_client.set(productName, productQuantity, function (err, reply) {
         if (err) {
             console.log(err);
-            res.send({ "operation_successful": false });
+            response.error = err;
+            res.send(response);
         }
 
         // insert into mysql
         else {
+            const data = {
+                name: productName,
+                quantity: productQuantity
+            };
+
             pool.query('INSERT INTO products SET ?', data, function (err, result) {
                 if (err) {
                     console.log(err);
-                    res.send({ "operation_successful": false });
+                    response.error = err;
+                    res.send(response);
                 }
                 else {
-                    console.log(result);
-                    res.send({ "operation_successful": true });
+                    response.operation_successful = true;
+                    res.send(response);
                 }
             })
         }
     });
 });
 
+app.post('/get', (req, res) => {
+    let response = [];
+
+    pool.query('SELECT * FROM products', function (err, results, fields) {
+        response[0] = results;
+        res.send(results);
+        // redis_client.get("test", function (err, data) {
+        //     console.log(data);
+        //     response[1] = data;
+        //     res.send(response);
+        // })
+
+    });
+});
+
+
 app.listen(port, () => {
-    console.log(`Inventory is not listening at http://localhost:${port}`);
+    console.log(`Inventory is listening at http://localhost:${port}`);
 });
